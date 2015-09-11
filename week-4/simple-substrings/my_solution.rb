808066f9fc9fe23d273c53ed3d5aa2f59961217d
David OKeefe <davidpokeefe3@gmail.com>
2015-09-10 22:56:24 -0400

    Add Detecting Simple Substrings Solution

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,14 @@
+# Simple Substrings
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def welcome(addr)
+  if addr =~ /CA/
+    "Welcome to California"
+  else
+    "You should move to California"
+  end
+end
