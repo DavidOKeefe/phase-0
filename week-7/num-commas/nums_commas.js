382a1ee53d97ed9289cef6fe0174d284e4fa0c4b
David OKeefe <davidpokeefe3@gmail.com>
2015-10-01 09:44:58 -0400

    Add Reflection

--- week-7/num-commas/nums_commas.js
+++ week-7/num-commas/nums_commas.js
@@ -1,10 +1,18 @@
 // Separate Numbers with Commas in JavaScript **Pairing Challenge**
 
 
-// I worked on this challenge with: .
+// I worked on this challenge with: Kai Huang.
 
 // Pseudocode
-
+// Convert the number to an array of string.
+// Reverse the array.
+// Create a new variable to hold the new number
+// Iterate through the array and slice into groups of three.
+// Put each slice into the new variable and
+// Add "," after each group.
+// Put the last element(s) in to the new variable.
+// Reverse the new variable and connect the numbers
+// and commas inside it.
 
 
 // Initial Solution
@@ -56,3 +64,18 @@ console.log(separateComma(10000000))
 
 
 // Reflection
+
+// Q: What was it like to approach the problem from the perspective of JavaScript?
+//    Did you approach the problem differently?
+// A: It was easier to come up with the logic the second time around but we were less
+//    familiar with JS methods so that research took some time.
+
+// Q: What did you learn about iterating over arrays in JavaScript?
+// A: There are fewer options than in ruby and it seems more manual.
+// Q: What was different about solving this problem in JavaScript?
+// A: The iteration options made it less succinct.
+// Q: What built-in methods did you find to incorporate in your refactored solution?
+// A: We used the .split(), .reverse(), .toString(), .push, .concat, .apply methods.
+
+
+
