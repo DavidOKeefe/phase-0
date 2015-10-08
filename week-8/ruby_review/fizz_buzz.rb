e9deec7a7e42c3738e7360477ab5689de5be3842
David OKeefe <davidpokeefe3@gmail.com>
2015-10-07 20:54:40 -0400

    Add super fizz buzz solution

---
+++ week-8/ruby_review/fizz_buzz.rb
@@ -0,0 +1,32 @@
+# I worked on this challenge [by myself, with: ].
+# This challenge took me [#] hours.
+
+
+# Pseudocode
+
+
+
+# Initial Solution
+
+def super_fizzbuzz(array)
+  array.map do |value|
+    if (value % 3 == 0 && value % 5 == 0)
+      'FizzBuzz'
+    elsif (value % 3 == 0)
+      'Fizz'
+    elsif (value % 5 == 0)
+      'Buzz'
+    else
+      value
+    end
+  end
+end
+
+# Refactored Solution
+
+
+
+
+
+
+# Reflection
