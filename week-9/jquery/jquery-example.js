bd235dbca384122b88fa07a886368b48afd222b1
David OKeefe <davidpokeefe3@gmail.com>
2015-10-13 21:37:04 -0400

    Add jQuery Initial Solution

--- week-9/jquery/jquery-example.js
+++ week-9/jquery/jquery-example.js
@@ -1,37 +1,37 @@
 // U3.W9:JQuery
 
 
-// I worked on this challenge [by myself, with: ].
+// I worked on this challenge by myself.
 // This challenge took me [#] hours.
 
 $(document).ready(function(){
 
-//RELEASE 0:
+//RELEASE 1:
   //link the image
 
-//RELEASE 1:
+//RELEASE 2:
 
   //Link this script and the jQuery library to the jQuery_example.html file and analyze what this code does.
 
 $('body').css({'background-color': 'pink'})
 
-//RELEASE 2:
-  //Add code here to select elements of the DOM
-
-
 //RELEASE 3:
+  //Add code here to select elements of the DOM
+  bodyElement = $('body');
+
+//RELEASE 4:
   // Add code here to modify the css and html of DOM elements
+  bodyHeaderOne = $('.header')
+  bodyHeaderOne.css('color', '#ddffdd')
+  bodyHeaderOne.css({"border-color": "#C1E0FF",
+             "border-weight":"1px",
+             "border-style":"solid"})
 
-
-//RELEASE 4: Event Listener
+//RELEASE 5: Event Listener
   // Add the code for the event listener here
-
-
-//RELEASE 5: Experiment on your own
-
-
-
-
-
+$('img').on('mouseover',function(e){
+  e.preventDefault()
+  $(this).attr('src', 'imgs/bobolink.jpeg')
+})
 
 })  // end of the document.ready function: do not remove or write DOM manipulation below this.
